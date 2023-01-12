d391ed739e8c9bed413d7a949d64361994654aac
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-12 11:04:07 -0300

    feat: testa a função cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,27 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
+
+# 1 - Testar o construtor/inicializador do objeto Produto
+# 1 - Deve criar um novo produto com todos os atributos
+# corretamente preenchidos.
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pass
+    product = Product(
+        id=1,
+        nome_do_produto="Café",
+        nome_da_empresa="Nescafé",
+        data_de_fabricacao="01/01/2021",
+        data_de_validade="01/01/2022",
+        numero_de_serie="123456789",
+        instrucoes_de_armazenamento="em local seco",
+        )
+
+    # assert len(Product.__dict__) == 6
+    assert product.id == 1
+    assert product.nome_do_produto == "Café"
+    assert product.nome_da_empresa == "Nescafé"
+    assert product.data_de_fabricacao == "01/01/2021"
+    assert product.data_de_validade == "01/01/2022"
+    assert product.numero_de_serie == "123456789"
+    assert product.instrucoes_de_armazenamento == "em local seco"
