b5dff17b6c4899342176e3036ebe28b095ea4da9
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-18 20:01:41 -0300

    feat: Testa a geração de uma versão do relatório em cores

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -15,7 +15,6 @@ def test_decorar_relatorio():
             "data_de_validade": "2023-02-09",
         },
         {
-
             "nome_da_empresa": "Farinini",
             "nome_do_produto": "Sabão em pó",
             "data_de_fabricacao": "2022-04-04",
@@ -23,6 +22,12 @@ def test_decorar_relatorio():
         },
     ]
     result = report.generate(products_list)
-    assert "\033[32mData de fabricação mais antiga:\033[0m \033[36m2022-04-04\033[0m" in result
-    assert "\033[32mData de validade mais próxima:\033[0m \033[36m2023-02-09\033[0m" in result
-    assert "\033[32mEmpresa com mais produtos:\033[0m \033[31mFarinini\033[0m" in result
+    assert (
+        "\033[32mData de fabricação mais antiga:\033[0m \033[36m2022-04-04\033[0m" in result
+    )
+    assert (
+        "\033[32mData de validade mais próxima:\033[0m \033[36m2023-02-09\033[0m" in result
+    )
+    assert (
+        "\033[32mEmpresa com mais produtos:\033[0m \033[31mFarinini\033[0m" in result
+    )
