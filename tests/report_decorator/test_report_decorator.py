1048da31827937d40edc7e9896600cbb41cc6bc5
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-19 10:32:50 -0300

    refactor:Testa a geração de uma versão do relatório em cores

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -27,10 +27,13 @@ def test_decorar_relatorio():
     data_antiga = "Data de fabricação mais antiga:"
     data_validade = "Data de validade mais próxima:"
     empresa = "Empresa com mais produtos:"
+    data_a = "2022-04-04"
+    data_v = "2023-02-09"
+    emp = "Farinini"
 
-    assert f"\033[32m{data_antiga}\033[0m \033[36m2022-04-04\033[0m" in res
-    assert f"\033[32m{data_validade}\033[0m \033[36m2023-02-09\033[0m" in res
-    assert f"\033[32m{empresa}\033[0m \033[31mFarinini\033[0m" in res
+    assert f"\033[32m{data_antiga}\033[0m \033[36m{data_a}\033[0m" in res
+    assert f"\033[32m{data_validade}\033[0m \033[36m{data_v}\033[0m" in res
+    assert f"\033[32m{empresa}\033[0m \033[31m{emp}\033[0m" in res
 
 # não consigo quebras as linhas sem "quebrar" o código no github!
 
